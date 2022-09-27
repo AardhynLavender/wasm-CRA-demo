5040a04e73ff9d95faf722efebbe5fa2846414b7
Aardhynlavneder <laveat1@student.op.ac.nz>
2022-09-28 11:36:16 +1300

    fix(main): add include guards

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
+#ifdef __EMSCRIPTEN__
 #include <emscripten/emscripten.h>
 #include <emscripten/bind.h>
+#endif // __EMSCRIPTEN__
 #include <string>
 
 std::string HelloWorld(std::string name) { return "Greetings " + name + "!"; }
 
+#ifdef __EMSCRIPTEN__
 EMSCRIPTEN_BINDINGS(my_module) {
     emscripten::function("HelloWorld", &HelloWorld);
 }
+#endif // __EMSCRIPTEN__
